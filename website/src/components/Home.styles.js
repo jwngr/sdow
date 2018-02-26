7c7a4d515f0bdb94738b79b5e025c822d80f77f4
jwngr <wenger.jacob@gmail.com>
2018-02-26 11:12:52 -0500

    Add instruction modal and GitHub link

--- website/src/components/Home.styles.js
+++ website/src/components/Home.styles.js
@@ -1,3 +1,4 @@
+import ReactModal from 'react-modal';
 import styled from 'styled-components';
 
 export const Logo = styled.img`
@@ -11,6 +12,104 @@ export const Logo = styled.img`
 
   @media (max-width: 600px) {
     width: 70%;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Modal = styled(ReactModal)`
+  position: absolute;
+  top: 80px;
+  right: 20px;
+  width: 300px;
+  padding: 12px;
+  z-index: 10;
+  text-align: justify;
+  border: solid 3px ${(props) => props.theme.colors.darkGreen};
+  background: ${(props) => props.theme.colors.creme};
+
+  p {
+    font-size: 16px;
+    font-family: 'Quicksand';
+    line-height: 1.5;
+    margin-bottom: 12px;
+  }
+
+  p:last-of-type {
+    margin-bottom: 0;
+  }
+
+  a,
+  a:visited {
+    color: ${(props) => props.theme.colors.darkGreen};
+    transition: color 0.5s;
+  }
+
+  a:hover {
+    color: ${(props) => props.theme.colors.red};
+  }
+
+  @media (max-width: 600px) {
+    width: 80%;
+    top: 104px;
+    left: 10%;
+  }
+`;
+
+export const IconLinks = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+    top: 28px;
+  }
+
+  @media (max-width: 600px) {
+    position: initial;
+    flex-direction: row;
+  }
+`;
+
+const SvgIcon = styled.svg`
+  cursor: pointer;
+  fill: ${(props) => props.theme.colors.darkGreen};
+  stroke: ${(props) => props.theme.colors.darkGreen};
+  transition: fill 0.5s, stroke 0.5s;
+
+  &:hover {
+    opacity: 1;
+    fill: ${(props) => props.theme.colors.red};
+    stroke: ${(props) => props.theme.colors.red};
+  }
+`;
+
+export const InfoIcon = SvgIcon.extend`
+  width: 44px;
+  height: 44px;
+  margin: -4px 12px 0 0;
+
+  @media (max-width: 1200px) {
+    margin: 0 0 8px -4px;
+  }
+
+  @media (max-width: 600px) {
+    width: 38px;
+    height: 38px;
+    margin: -4px 12px 0 0;
+  }
+`;
+
+export const GitHubLogo = SvgIcon.extend`
+  width: 36px;
+  height: 36px;
+
+  @media (max-width: 600px) {
+    width: 30px;
+    height: 30px;
   }
 `;
 
