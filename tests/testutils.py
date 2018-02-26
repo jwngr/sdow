1fca15b14c5163427c1e775c75fa1f9376aa1faf
jwngr <wenger.jacob@gmail.com>
2018-02-26 13:30:32 -0500

    Remove tests and linting

--- tests/testutils.py
+++
@@ -1,3 +0,0 @@
-def assert_array_equals(array_1, array_2):
-  """Asserts that array_1 and array_2 contain the same items."""
-  assert len(array_1) == len(array_2) and sorted(array_1) == sorted(array_2)
