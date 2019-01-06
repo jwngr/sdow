40e6d90286092ba927ce00f8d35faa8688ffbb00
jwngr <wenger.jacob@gmail.com>
2019-01-06 10:46:37 +0100

    Add missing key prop to blog post list

--- website/src/components/blog/Blog/index.js
+++ website/src/components/blog/Blog/index.js
@@ -37,7 +37,7 @@ export default () => (
 
       <Divider />
       {posts.map((postInfo) => (
-        <React.Fragment>
+        <React.Fragment key={postInfo.id}>
           <BlogPostCard {...postInfo} />
           <Divider />
         </React.Fragment>
