9b3e10a41197119d6b3a5f39997cae0b9aebe68a
jwngr <wenger.jacob@gmail.com>
2019-02-09 11:57:26 +0000

    Added access log and capture debug output in Gunicorn

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -3,4 +3,6 @@ import multiprocessing
 name = 'sdow'
 bind = '127.0.0.1:8000'
 errorlog = 'errors.log'
+accesslog = 'access.log'
+capture_output = True
 workers = multiprocessing.cpu_count() * 2 + 1
