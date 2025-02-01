ed2b72973bc15be63aa677963123ca51f7915917
jwngr <wenger.jacob@gmail.com>
2025-02-01 00:29:42 -0800

    Fixed lint

--- website/src/components/ResultsList.tsx
+++ website/src/components/ResultsList.tsx
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import React, {Suspense} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import defaultPageThumbnail from '../images/defaultPageThumbnail.png';
